8b369d8f01f1ab626c774171a742366e9a45cbad
Andrew B Coathup <andrew@coathup.net>
2019-03-04 12:31:05 +1100

    Add save to contract

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
-//import EmbarkJS from 'Embark/EmbarkJS';
-
-// import your contracts
-//import EtherPost from 'Embark/contracts/EtherPost';
-
-// import web3
-//var Web3 = require('web3')
-
 // import choo
 var choo = require('choo')
-
 // import template
 var main = require('./templates/main');
+// import web3
+var Web3 = require('web3')
+// Import contract ABI
+var contractABI = require("./dist/contracts/EtherPost.json").abiDefinition
+// Buffer for files
+var buffer = require('buffer')
+// bs58
+var bs58 = require('bs58')
 
 // initialize choo
 var app = choo()
-
-// Buffer for files
-var buffer = require('buffer')
 
 // Initialize IPFS
 var IPFS = require('ipfs-http-client')
 var node = new IPFS('ipfs.infura.io', '5001', {protocol: 'https'})
 
 app.use(function (state, emitter) {
+
+    emitter.on('DOMContentLoaded', async () => {
+        // Check for web3 instance. Create if necessary.
+        // Access MetaMask
+        if (window.ethereum) {
+            try {
+                await window.ethereum.enable()
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        // Set up web3 provider
+        web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8555"))
+
+        // Set up contract interface
+        state.contractInstance = new web3.eth.Contract(contractABI, "0x04D45b51fe4f00b4478F8b0719Fa779f14c8A194")
+    
+        // Unlock account
+        const accounts = await web3.eth.getAccounts()
+        web3.eth.personal.unlockAccount(accounts[0], async function (error, result) {
+            if (error) {
+                console.error(error)
+            }
+            else {
+                web3.eth.defaultAccount = accounts[0]
+            }
+        });
+    })
+
     emitter.on('upload', function (file) {
         const reader = new FileReader();
         reader.onloadend = function () {
@@ -32,9 +58,17 @@ app.use(function (state, emitter) {
                     console.error(err)
                     return
                 }
+                var ipfsHash = result[0].hash;
+
                 state.ipfsUrl = `https://ipfs.io/ipfs/${result[0].hash}`
                 console.log(state.ipfsUrl)
                 emitter.emit('render')
+
+                state.contractInstance.methods.upload(getBytes32FromIpfsHash(ipfsHash)).send({ from: web3.eth.defaultAccount })
+                .on('error', console.error)
+                .on('receipt', async receipt => {
+                    console.log("Saved to smart contract", receipt)
+                })
             })
         }
 
@@ -48,3 +82,17 @@ app.route('/', main)
 // start app
 app.mount('div')
 
+// Return bytes32 hex string from IPFS hash
+function getBytes32FromIpfsHash(ipfsHash) {
+  return "0x" + bs58.decode(ipfsHash).slice(2).toString('hex')
+ }
+  
+// Return base58 encoded ipfs hash from bytes32 hex string
+function getIpfsHashFromBytes32(bytes32Hex) {
+  // Add default ipfs values for first 2 bytes: function: 0x12=sha2, size: 0x20=256 bits
+  // Cut off leading "0x"
+  const hex = "1220" + bytes32Hex.slice(2)
+  const hashBytes = Buffer.from(hex, 'hex');
+  const str = bs58.encode(hashBytes)
+  return str
+}
