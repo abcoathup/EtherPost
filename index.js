87134a3dd50cc8d39db89bf898fba54b0f2edaae
Andrew B Coathup <andrew@coathup.net>
2019-03-07 12:30:39 +1100

    Refactoring to rename functions

--- index.js
+++ index.js
@@ -38,52 +38,6 @@ app.use(function (state, emitter) {
 
         // Set up contract interface
         state.contractInstance = new web3.eth.Contract(contractABI, "0x04D45b51fe4f00b4478F8b0719Fa779f14c8A194")
-        await getUploads(state);
-        emitter.emit('render');
-
-        await updateState(state);
-        emitter.emit('render');
-
-        state.contractInstance.events.LogUpload((err, event) => {
-            if (err) {
-              // something went wrong
-              console.log(err);
-            } 
-            const uploader = event.returnValues.uploader;
-            var ipfsHash = getIpfsHashFromBytes32(event.returnValues.ipfsHash);
-            
-            var upload = { ipfsHash: ipfsHash, clapCount: 0, comments: [] };
-            state.uploads.push(upload);
-            emitter.emit('render')
-        })
-        
-        state.contractInstance.events.LogClap(async (err, event) => {
-            if (err) {
-              // something went wrong
-              console.log(err);
-            } 
-            const clapper = event.returnValues.clapper;
-            var ipfsHash = getIpfsHashFromBytes32(event.returnValues.ipfsHash);
-            
-            await updateStateClapCount(state, ipfsHash);
-            
-            emitter.emit('render')
-        })
-
-        state.contractInstance.events.LogComment(async (err, event) => {
-            if (err) {
-              // something went wrong
-              console.log(err);
-            } 
-
-            const commenter = event.returnValues.commenter;
-            var imageHash = getIpfsHashFromBytes32(event.returnValues.imageHash);
-            var commentHash = getIpfsHashFromBytes32(event.returnValues.commentHash);
-            const timestamp = event.returnValues.timestamp;
-            await updateStateComments(state, imageHash);
-            
-            emitter.emit('render')
-        })
 
         // Unlock account
         const accounts = await web3.eth.getAccounts()
@@ -95,8 +49,61 @@ app.use(function (state, emitter) {
                 web3.eth.defaultAccount = accounts[0]
             }
         });
+        
+        // set state uploads first to display
+        await setStateUploads(state);
+        emitter.emit('render');
+
+        // set state claps and comments
+        await setStateClapsAndComments(state);
+        emitter.emit('render');
+
+        // Listen for LogUpload smart contract event and update dApp with upload
+        state.contractInstance.events.LogUpload((err, event) => {
+            if (err) {
+              console.log(err);
+            } 
+            
+            const uploader = event.returnValues.uploader;
+            var ipfsHash = getIpfsHashFromBytes32(event.returnValues.ipfsHash);
+            
+            var upload = { ipfsHash: ipfsHash, clapCount: 0, comments: [] };
+            state.uploads.push(upload);
+            emitter.emit('render')
+        })
+        
+        // Listen for LogUpload smart contract event and update dApp with clap
+        state.contractInstance.events.LogClap(async (err, event) => {
+            if (err) {
+              console.log(err);
+            } 
+            
+            const clapper = event.returnValues.clapper;
+            var ipfsHash = getIpfsHashFromBytes32(event.returnValues.ipfsHash);
+            
+            await setStateClapCount(state, ipfsHash);
+            
+            emitter.emit('render')
+        })
+
+        // Listen for LogComment smart contract event and update dApp with comment
+        state.contractInstance.events.LogComment(async (err, event) => {
+            if (err) {
+              console.log(err);
+            } 
+
+            const commenter = event.returnValues.commenter;
+            var imageHash = getIpfsHashFromBytes32(event.returnValues.imageHash);
+            var commentHash = getIpfsHashFromBytes32(event.returnValues.commentHash);
+            const timestamp = event.returnValues.timestamp;
+            
+            await setStateComments(state, imageHash);
+            
+            emitter.emit('render')
+        })
     })
 
+    // Listen for upload dApp event and save upload to smart contract
     emitter.on('upload', function (file) {
         console.log('Upload file to IPFS: ', file.name)
         const reader = new FileReader();
@@ -112,7 +119,7 @@ app.use(function (state, emitter) {
                 state.contractInstance.methods.upload(getBytes32FromIpfsHash(ipfsHash)).send({ from: web3.eth.defaultAccount })
                 .on('error', console.error)
                 .on('receipt', async receipt => {
-                    console.log("Saved upload to smart contract: ", ipfsHash)
+                    console.log("Saved upload to smart contract with ipfsHash: ", ipfsHash)
                 })
             })
         }
@@ -120,14 +127,16 @@ app.use(function (state, emitter) {
         reader.readAsArrayBuffer(file)
     })
 
+    // Listen for clap dApp event and save clap to smart contract
     emitter.on('clap', function (ipfsHash) {
         state.contractInstance.methods.clap(getBytes32FromIpfsHash(ipfsHash)).send({ from: web3.eth.defaultAccount })
             .on('error', console.error)
             .on('receipt', async receipt => {
-                console.log("Saved clap to smart contract: ", ipfsHash)
+                console.log("Saved clap to smart contract for upload with ipfsHash: ", ipfsHash)
             })
     })
 
+    // Listen for comment dApp event and save comment to smart contract
     emitter.on('comment', function (data) {
         var imageHash = data.ipfsHash;
         console.log('Upload comment to IPFS: ', data.comment)
@@ -143,7 +152,7 @@ app.use(function (state, emitter) {
             state.contractInstance.methods.comment(getBytes32FromIpfsHash(imageHash), getBytes32FromIpfsHash(commentHash)).send({ from: web3.eth.defaultAccount })
                 .on('error', console.error)
                 .on('receipt', async receipt => {
-                    console.log("Saved comment to smart contract: ", commentHash)
+                    console.log("Saved comment to smart contract with ipfsHash: ", commentHash)
             })
         })
     })
@@ -170,6 +179,7 @@ function getIpfsHashFromBytes32(bytes32Hex) {
   return str
 }
 
+// Return clap count from smart contract for given upload ipfsHash
 function getClapCount(state, ipfsHash) {
     return new Promise(function (resolve, reject) {
         state.contractInstance.methods.getClapCount(getBytes32FromIpfsHash(ipfsHash)).call().then(function (response) {
@@ -178,6 +188,7 @@ function getClapCount(state, ipfsHash) {
     });
 }
 
+// Return comments from smart contract for given upload ipfsHash
 function getComments(state, ipfsHash) {
     return new Promise(function (resolve, reject) {
         state.contractInstance.methods.getComments(getBytes32FromIpfsHash(ipfsHash)).call().then(function (response) {
@@ -186,7 +197,7 @@ function getComments(state, ipfsHash) {
     });
 }
 
-
+// Return uploads from smart contract for given user
 function getUploadsForUser(state, user) {
     return new Promise(function (resolve, reject) {
         state.contractInstance.methods.getUploads(user).call().then(function (response) {
@@ -195,7 +206,7 @@ function getUploadsForUser(state, user) {
     });
 }
 
-async function getUploads(state) {
+async function setStateUploads(state) {
     const accounts = await web3.eth.getAccounts();
     var uploads = await getUploadsForUser(state, accounts[0]);
     
@@ -207,7 +218,7 @@ async function getUploads(state) {
     });
 }
 
-async function updateStateClapCount(state, ipfsHash) {
+async function setStateClapCount(state, ipfsHash) {
     var clapCount = await getClapCount(state, ipfsHash);
     for(var index = 0; index < state.uploads.length; index++) {
         if (state.uploads[index].ipfsHash == ipfsHash) {
@@ -216,7 +227,7 @@ async function updateStateClapCount(state, ipfsHash) {
     }
 }
 
-async function updateStateComments(state, imageHash) {
+async function setStateComments(state, imageHash) {
     var commentsIpfsHashes = await getComments(state, imageHash);
     var comments = [];
 
@@ -235,7 +246,7 @@ async function updateStateComments(state, imageHash) {
     }
 }
 
-async function updateState(state) {
+async function setStateClapsAndComments(state) {
     for(var index = 0; index < state.uploads.length; index++) {
         var clapCount = await getClapCount(state, state.uploads[index].ipfsHash);
         var commentsIpfsHashes = await getComments(state, state.uploads[index].ipfsHash)
